d1cd9d6285438a2565b019ff426dc9f823010306
JsExpertCoder <misterjs247000@gmail.com>
2025-01-05 23:04:48 +0100

    create the compiler script

--- c/exe/c09/ex00/libft_creator.sh
+++ c/exe/c09/ex00/libft_creator.sh
@@ -1,1 +1,16 @@
 #!/bin/sh
+
+# source_files=$(ls *.c)
+source_files="ft_putchar.c ft_strcmp.c ft_swap.c ft_putstr.c ft_strlen.c"
+
+# object_files=$(echo $source_files | sed 's/\.c/\.o/g')
+object_files="ft_putchar.o ft_strcmp.o ft_swap.o ft_putstr.o ft_strlen.o"
+
+gcc -Wall -Wextra -Werror -c $source_files
+
+ar rcs libft.a $object_files
+
+rm $object_files
+
+
+
