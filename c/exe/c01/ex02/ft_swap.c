5f75687d256c21c8d9ae9b0f176673418a75895d
JsExpertCoder <misterjs247000@gmail.com>
2025-01-12 21:22:17 +0100

    c01: update ex02

--- c/exe/c01/ex02/ft_swap.c
+++ c/exe/c01/ex02/ft_swap.c
@@ -6,11 +6,9 @@
 /*   By: fnicolau <fnicolau@student.42luanda.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 19:57:06 by fnicolau          #+#    #+#             */
-/*   Updated: 2024/10/13 20:07:08 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/01/12 21:08:56 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 void	ft_swap(int *a, int *b)
 {
@@ -19,22 +17,4 @@ void	ft_swap(int *a, int *b)
 	aux = *a;
 	*a = *b;
 	*b = aux;
-}
-
-// remove this before submission!!
-int	main(void)
-{
-	int	x;
-	int	y;
-	int	*a;
-	int	*b;
-
-	x = 97;
-	y = 80;
-	a = &x;
-	b = &y;
-	printf("before: x -> %i y -> %i", x, y);
-	ft_swap(a, b);
-	printf("after x -> %i y -> %i", x, y);
-	return (0);
 }
