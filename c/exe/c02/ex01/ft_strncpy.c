fbd7875461082c8929791297f5e756a079ac2a53
JsExpertCoder <misterjs247000@gmail.com>
2025-01-14 05:14:12 +0100

    c02: add ex01

---
+++ c/exe/c02/ex01/ft_strncpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnicolau <marvin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/14 04:07:32 by fnicolau          #+#    #+#             */
+/*   Updated: 2025/01/14 05:02:35 by fnicolau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	char	*pt_dest;
+	unsigned int	i;
+
+	i = 0;
+	pt_dest = dest;
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+		dest[i++] = 0;
+	return (pt_dest);
+}
