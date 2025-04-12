115e706c22ce75d35415412005aadc6daf4d97a0
JsExpertCoder <misterjs247000@gmail.com>
2025-04-12 16:54:52 +0100

    fix(main.c): clean up error handling

--- c/exe/c10/ex01/src/main.c
+++ c/exe/c10/ex01/src/main.c
@@ -3,22 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fnicolau <marvin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fnicolau <fnicolau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 15:14:43 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/04/08 18:33:10 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/04/12 16:37:49 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_cat.h"
-
 int	main(int ac, char **av)
 {
-	int		i;
-	int		fd;
-	ssize_t	bytes_read;
-	int		final_return;
-	char	buffer[BUFFER_SIZE];
+	int	i;
+	int	fd;
+	int	final_return;
 
 	i = 1;
 	final_return = 0;
@@ -26,30 +22,10 @@ int	main(int ac, char **av)
 	{
 		fd = open(av[i], O_RDONLY);
 		if (fd == -1)
-		{
-			ft_putstr_fd("cat: ", 2);
-			ft_putstr_fd(av[i], 2);
-			ft_putstr_fd(": ", 2);
-			ft_putstr_fd(strerror(errno), 2);
-			ft_putstr_fd("\n", 2);
-			close(fd);
-			final_return = -1;
-		}
+			final_return = print_error(av[i], fd);
 		else
-		{
-			bytes_read = read(fd, &buffer, BUFFER_SIZE);
-			if (bytes_read == -1)
-			{
-				ft_putstr_fd(strerror(errno), 2);
-				close(fd);
+			if (ft_cat(av[i], fd) == -1)
 				final_return = -1;
-			}
-			else if (bytes_read > 0)
-			{
-				buffer[bytes_read] = 0;
-				ft_putstr_fd(buffer, 1);
-			}
-		}
 		close(fd);
 		i++;
 	}
