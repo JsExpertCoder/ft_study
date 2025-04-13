25098bd0e10f6571bec7b8ea4118bb3a0e7eedd5
JsExpertCoder <misterjs247000@gmail.com>
2025-04-13 23:24:50 +0100

    fix(ft_cat.h): justify includes

--- c/exe/c10/ex01/include/ft_cat.h
+++ c/exe/c10/ex01/include/ft_cat.h
@@ -6,7 +6,7 @@
 /*   By: fnicolau <fnicolau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 14:59:58 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/04/12 16:08:55 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/04/12 18:08:06 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 
 # define BUFFER_SIZE 3000
 
-# include <fcntl.h>
-# include <errno.h>
-# include <string.h>
-# include <unistd.h>
-# include <stdbool.h>
+# include <fcntl.h>		// for open()
+# include <unistd.h>	// for read(), write(), close()
+# include <errno.h>		// for errno
+# include <string.h>	// for strerror()
+# include <libgen.h>	// for basename()
+# include <stdbool.h>	// for "true", "false"
 
 int		ft_cat(char *file_path, int fd);
 void	ft_putstr_fd(char *str, int fd);
