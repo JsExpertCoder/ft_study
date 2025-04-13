7b9b3153e5310b9dbcf4e7e28b3abe3788146fde
JsExpertCoder <misterjs247000@gmail.com>
2025-04-13 23:27:32 +0100

    fix(print_error.c): update error return value

--- c/exe/c10/ex01/src/print_error.c
+++ c/exe/c10/ex01/src/print_error.c
@@ -6,7 +6,7 @@
 /*   By: fnicolau <fnicolau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 20:32:01 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/04/12 16:06:32 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/04/12 17:33:45 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,5 @@ int	print_error(char *file_path, int fd)
 	ft_putstr_fd(strerror(errno), 2);
 	ft_putstr_fd("\n", 2);
 	close(fd);
-	return (-1);
+	return (1);
 }
