0e07af0b05a5685af411c7cbf6dafb0d981c9b5e
JsExpertCoder <misterjs247000@gmail.com>
2025-04-13 23:27:08 +0100

    fix(main.c): update return handling and improve readability in main function

--- c/exe/c10/ex01/src/main.c
+++ c/exe/c10/ex01/src/main.c
@@ -6,7 +6,7 @@
 /*   By: fnicolau <fnicolau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 15:14:43 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/04/12 16:37:49 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/04/12 18:07:21 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@ int	main(int ac, char **av)
 	int	fd;
 	int	final_return;
 
-	i = 1;
 	final_return = 0;
+	if (ac == 1)
+		return (ft_cat("stdin", 0));
+	i = 1;
 	while (i < ac)
 	{
 		fd = open(av[i], O_RDONLY);
 		if (fd == -1)
 			final_return = print_error(av[i], fd);
 		else
-			if (ft_cat(av[i], fd) == -1)
-				final_return = -1;
-		close(fd);
+			final_return = ft_cat(av[i], fd);
 		i++;
 	}
 	return (final_return);
