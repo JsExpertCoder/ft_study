7ec09dcf428a9e4f4eb7cd49e7bc7d1d7ca40898
JsExpertCoder <misterjs247000@gmail.com>
2025-04-15 20:09:32 +0100

    refactor(ft_tail.c): simplify function parameters and update output messages

--- c/exe/c10/ex02/src/ft_tail.c
+++ c/exe/c10/ex02/src/ft_tail.c
@@ -6,29 +6,22 @@
 /*   By: fnicolau <fnicolau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 19:03:45 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/04/14 20:23:27 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/04/15 19:42:05 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_tail.h"
 
-int	ft_tail(char *file_path, int fd, size_t bytes_to_read,	\
-			unsigned int lines_to_read)
+int	ft_tail(char *file_path, int fd, size_t bytes_to_read)
 {
 	(void)fd;
 	(void)file_path;
 	(void)bytes_to_read;
-	(void)lines_to_read;
-	if (lines_to_read > 0)
-	{
-		ft_putstr_fd("ft_tail: read 10 last lines of fd\n", 1);
-		return (0);
-	}
-	else if (ft_strcmp(file_path, "stdin") == 0)
+	if (fd == 0)
 	{
 		ft_putstr_fd("ft_tail: read all BYTES of fd\n", 1);
 		return (0);
 	}
-	ft_putstr_fd("ft_tail: read n BYTES of fd from end\n", 1);
+	ft_putstr_fd("ft_tail: read bytes_to_read of fd from end\n", 1);
 	return (1);
 }
