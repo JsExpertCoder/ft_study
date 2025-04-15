fbc084d3b48554b7bab6c443468a1a6b53580e07
JsExpertCoder <misterjs247000@gmail.com>
2025-04-15 20:09:19 +0100

    fix(ft_tail.h): update function declarations and fix formatting issues

--- c/exe/c10/ex02/include/ft_tail.h
+++ c/exe/c10/ex02/include/ft_tail.h
@@ -6,7 +6,7 @@
 /*   By: fnicolau <fnicolau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:49:24 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/04/14 20:10:27 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/04/15 19:42:19 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,14 @@
 # include <libgen.h>	// for basename()
 # include <stdbool.h>	// for "true", "false"
 
-int		option_parser(char *option);
+int		ft_atoi(const char *nptr);
+size_t	ft_strlen(const char *s);
 void	ft_putstr_fd(char *str, int fd);
 void	ft_putchar_fd(const char c, int fd);
 int		print_error(char *file_path, int fd);
+int		option_parser(char **args, size_t length);
 int		ft_strcmp(const char *s1, const char *s2);
-int		ft_tail(char *fp, int fd, size_t bytes_to_read,	\
-				unsigned int lines_to_read);
+int		ft_tail(char *file_path, int fd, size_t bytes_to_read);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
