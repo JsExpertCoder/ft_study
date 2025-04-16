a4aa3ceb905cac0a37eea81fb16fbe9398cebd73
JsExpertCoder <misterjs247000@gmail.com>
2025-04-16 21:52:41 +0100

    fix(print_error.c): correct author email and improve file descriptor handling

--- c/exe/c10/ex02/src/print_error.c
+++ c/exe/c10/ex02/src/print_error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print_error.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fnicolau <fnicolau@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fnicolau <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 20:05:11 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/04/14 20:05:17 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/04/16 20:40:45 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int	print_error(char *file_path, int fd)
 	ft_putstr_fd("' for reading: ", 2);
 	ft_putstr_fd(strerror(errno), 2);
 	ft_putstr_fd("\n", 2);
-	close(fd);
+	if (fd > 2)
+		close(fd);
 	return (1);
 }
