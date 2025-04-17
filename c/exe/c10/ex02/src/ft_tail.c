7f0b97c2856eb0c8a7833a23c29fc4e4afbc0672
JsExpertCoder <misterjs247000@gmail.com>
2025-04-17 22:00:04 +0100

    refactor(ft_tail.c): improve file size calculation and buffer handling

--- c/exe/c10/ex02/src/ft_tail.c
+++ c/exe/c10/ex02/src/ft_tail.c
@@ -12,35 +12,62 @@
 
 #include "ft_tail.h"
 
+static int	get_file_size(char *file_path)
+{
+	int		fd;
+	char	buffer;
+	int		file_size;
+	ssize_t	bytes_read;
+
+	fd = open(file_path, O_RDONLY);
+	if (fd == -1)
+		return (print_error(file_path, fd) *-1);
+	file_size = 0;
+	while (true)
+	{
+		bytes_read = read(fd, &buffer, 1);
+		if (bytes_read == -1)
+		{
+			print_error(file_path, fd);
+			return (-1);
+		}
+		else if (bytes_read == 0)
+			break ;
+		file_size++;
+	}
+	return (file_size);
+}
+
 int	ft_tail(char *file_path, int fd, size_t bytes_to_read)
 {
 	char	*buffer;
+	int		file_size;
 	ssize_t	bytes_read;
-	ssize_t	last_total_bytes_read;
 
-	if (fd == 0)
+	(void)bytes_to_read;
+	if (fd > 2)
 	{
-		buffer = (char *)malloc(bytes_to_read + 1);
+		file_size = get_file_size(file_path);
+		if (file_size == -1)
+			return (1);
+		buffer = (char *)malloc(file_size + 1);
 		if (buffer == NULL)
 			return (1);
-		while (true)
+		bytes_read = read(fd, buffer, file_size);
+		if (bytes_read == -1)
 		{
-			bytes_read = read(fd, buffer, bytes_to_read);
-			if (bytes_read == -1)
-			{
-				free(buffer);
-				print_error(file_path, fd);
-				return (1);
-			}
-			if (bytes_read > 0)
-				last_total_bytes_read = bytes_read;
-			else
-			{
-				write(1, buffer, last_total_bytes_read);
-				free(buffer);
-				return (0);
-			}
+			free(buffer);
+			print_error(file_path, fd);
+			return (1);
+		}
+		else if (bytes_read > 0)
+		{
+			buffer[file_size] = 0;
+			ft_putstr_fd(&buffer[file_size - bytes_to_read], 1);
+			free(buffer);
 		}
 	}
 	return (0);
 }
+
+// use a circular buffer implementation later
