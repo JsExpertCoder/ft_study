2a6f772d9c44bf47198276d5582505c53ba70bcb
JsExpertCoder <misterjs247000@gmail.com>
2025-04-17 21:58:12 +0100

    refactor(main.c): update file parsing logic and clean up unused variables

--- c/exe/c10/ex02/src/main.c
+++ c/exe/c10/ex02/src/main.c
@@ -6,7 +6,7 @@
 /*   By: fnicolau <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:47:38 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/04/15 20:09:52 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/04/17 21:50:21 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,33 @@
 
 int	main(int argc, char **argv)
 {
-	int		fd;
-	int		i;
+	int		j;
+	char	**files;
 	int		final_return;
 	int		bytes_to_read;
-	int		total_of_files;
 
 	final_return = 0;
-	total_of_files = 0;
 	bytes_to_read = option_parser(argv, argc);
 	if (bytes_to_read == -1)
 		return (-1);
 	else if (bytes_to_read)
 	{
-		i = 1;
-		while (argv[i])
+		printf("> bytes to read: %i\n", bytes_to_read);
+		files = files_parser(argv + 1);
+		if (files == NULL)
+			return (1);
+		j = 0;
+		while (files[j])
 		{
-			if (argv[i][0] != '-')
-			{
-				fd = open(argv[i], O_RDONLY);
-				if (fd == -1)
-				{
-					print_error(argv[i], fd);
-					final_return = -1;
-				}
-				else
-					ft_tail(argv[i], fd, bytes_to_read);
-				total_of_files++;
-			}
-			i++;
+			printf("> files-%i: %s\n", j, files[j]);
+			j++;
 		}
-		if (total_of_files == 0)
-			final_return = ft_tail("stdin", 0, bytes_to_read);
+		free(files);
 	}
 	return (final_return);
 }
+
+// int	main(void)
+// {
+// 	return (0);
+// }
