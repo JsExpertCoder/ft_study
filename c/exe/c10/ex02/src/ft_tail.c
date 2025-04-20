b637f24c6e9fb9e805b251cffc0cddfee95c3339
JsExpertCoder <misterjs247000@gmail.com>
2025-04-20 20:38:04 +0100

    refactor(ft_tail.c): simplify file reading logic using a circular buffer

--- c/exe/c10/ex02/src/ft_tail.c
+++ c/exe/c10/ex02/src/ft_tail.c
@@ -6,68 +6,73 @@
 /*   By: fnicolau <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 19:03:45 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/04/17 22:51:15 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/04/20 20:36:47 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_tail.h"
 
-static int	get_file_size(char *file_path)
+static void	print_circular_buffer(char *c_buffer, size_t cb_index,	\
+	size_t bytes_to_read)
 {
-	int		fd;
-	char	buffer;
-	int		file_size;
-	ssize_t	bytes_read;
+	size_t	last_cb_index;
 
-	fd = open(file_path, O_RDONLY);
-	if (fd == -1)
-		return (print_error(file_path, fd) *-1);
-	file_size = 0;
-	while (true)
+	if ((cb_index + 1) == bytes_to_read)
+		write(1, c_buffer, bytes_to_read);
+	else
 	{
-		bytes_read = read(fd, &buffer, 1);
-		if (bytes_read == -1)
+		last_cb_index = cb_index;
+		while (true)
 		{
-			print_error(file_path, fd);
-			return (-1);
+			cb_index = (cb_index + 1) % bytes_to_read;
+			write(1, &c_buffer[cb_index], 1);
+			if (last_cb_index == cb_index)
+				break ;
 		}
-		else if (bytes_read == 0)
-			break ;
-		file_size++;
 	}
-	return (file_size);
 }
 
 int	ft_tail(char *file_path, int fd, size_t bytes_to_read)
 {
-	char	*buffer;
-	int		file_size;
+	ssize_t	i;
 	ssize_t	bytes_read;
+	char	*circular_buffer;
+	size_t	total_bytes_read;
+	char	temp_buffer[3000];
+	size_t	circular_buf_index;
 
-	(void)bytes_to_read;
-	if (fd > 2)
+	circular_buffer = malloc((bytes_to_read));
+	if (!circular_buffer)
+		return (1);
+	total_bytes_read = 0;
+	circular_buf_index = 0;
+	while (true)
 	{
-		file_size = get_file_size(file_path);
-		if (file_size == -1)
-			return (1);
-		buffer = malloc(file_size + 1);
-		if (buffer == NULL)
-			return (1);
-		bytes_read = read(fd, buffer, file_size);
+		bytes_read = read(fd, temp_buffer, sizeof(temp_buffer));
 		if (bytes_read == -1)
 		{
-			free(buffer);
-			print_error(file_path, fd);
-			return (1);
+			free(circular_buffer);
+			return (print_error(file_path, fd));
 		}
 		else if (bytes_read > 0)
 		{
-			buffer[file_size] = 0;
-			ft_putstr_fd(&buffer[file_size - bytes_to_read], 1);
-			free(buffer);
+			total_bytes_read += bytes_read;
+			i = 0;
+			while (i < bytes_read)
+			{
+				circular_buffer[circular_buf_index] = temp_buffer[i];
+				i++;
+				if (i < bytes_read)
+					circular_buf_index = (circular_buf_index + 1) % bytes_to_read;
+			}
 		}
+		else
+			break ;
 	}
+	if (total_bytes_read >= bytes_to_read)
+		print_circular_buffer(circular_buffer, circular_buf_index, bytes_to_read);
+	else
+		write(1, circular_buffer, total_bytes_read);
+	free(circular_buffer);
 	return (0);
 }
-
-// use a circular buffer implementation later
