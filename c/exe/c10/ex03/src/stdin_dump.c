0a3570e85842081ddbfc56753d28662e8ff19f36
JsExpertCoder <misterjs247000@gmail.com>
2025-04-30 20:52:58 +0100

    feat: implement print_offset function for hexadecimal formatting in stdin_dump

--- c/exe/c10/ex03/src/stdin_dump.c
+++ c/exe/c10/ex03/src/stdin_dump.c
@@ -6,27 +6,56 @@
 /*   By: fnicolau <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 14:50:26 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/04/28 17:07:58 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/04/30 20:36:17 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_hexdump.h"
 
+static void	print_offset(size_t nbr)
+{
+	size_t	i;
+	char	result[8];
+	char	*hexadecimals;
+
+	i = 0;
+	hexadecimals = "0123456789abcdef";
+	while (i < sizeof(result))
+	{
+		result[i] = '0';
+		i++;
+	}
+	while (nbr > 0 && i > 0)
+	{
+		result[i - 1] = hexadecimals[nbr % 16];
+		i--;
+		nbr /= 16;
+	}
+	ft_putstr_fd(result, 1);
+	ft_putstr_fd(" ", 1);
+	return ;
+}
+
 unsigned int	stdin_dump(bool use_canonical_style)
 {
+	size_t	offset;
 	ssize_t	bytes_read;
-	char	temp_buffer[3000];
+	char	temp_buffer[16];
 
-	bytes_read = read(STDIN, temp_buffer, sizeof(temp_buffer));
+	offset = 0;
+	(void)use_canonical_style;
+	bytes_read = read(STDIN, temp_buffer, sizeof(temp_buffer) - 1);
+	while (bytes_read > 0)
+	{
+		print_offset(offset);
+		ft_putstr_fd("\n", 1);
+		offset += 16;
+		bytes_read = read(STDIN, temp_buffer, sizeof(temp_buffer));
+	}
 	if (bytes_read == -1)
 	{
 		print_error(0, NULL, errno);
 		return (1);
 	}
-	if (use_canonical_style)
-		ft_putstr_fd("> -- canonical on stdin:\n", 1);
-	else
-		ft_putstr_fd("> -- default on stdin:\n", 1);
-	write(1, temp_buffer, bytes_read);
 	return (0);
 }
