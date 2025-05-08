2e6b80770c9568655de6e3a3c1f7f30695a4c082
JsExpertCoder <misterjs247000@gmail.com>
2025-05-08 22:47:10 +0100

    feat: implement buffer initialization and final print functions in main.c

--- c/exe/c10/ex03/src/main.c
+++ c/exe/c10/ex03/src/main.c
@@ -6,7 +6,7 @@
 /*   By: fnicolau <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 18:37:15 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/04/28 16:44:26 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/05/03 15:32:47 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,23 +29,71 @@ static bool	check_option_existence(char **tokens)
 	return (false);
 }
 
+t_buffer	*init_buffer(void)
+{
+	size_t		i;
+	size_t		size;
+	t_buffer	*buffer;
+
+	buffer = malloc(sizeof(t_buffer));
+	if (!buffer)
+		return (NULL);
+	size = 16;
+	buffer->current = malloc(size + 1);
+	buffer->previous = malloc(size + 1);
+	if (!buffer->current || !buffer->previous)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	buffer->offset = 0;
+	buffer->bytes_rd = 0;
+	buffer->total_bytes_rd = 0;
+	i = 0;
+	while (i++ <= size)
+	{
+		buffer->current[i - 1] = 0;
+		buffer->previous[i - 1] = 0;
+	}
+	return (buffer);
+}
+
+void	final_print(t_buffer *buffer, bool canonical_style)
+{
+	if (buffer->bytes_rd > 0)
+	{
+		ft_hexdump(buffer, canonical_style);
+		print_hexa(8, buffer->total_bytes_rd);
+		write(1, "\n", 1);
+	}
+	return ;
+}
+
 int	main(int argc, char **argv)
 {
-	t_files	*files;
-	size_t	final_return;
-	bool	use_canonical_style;
+	t_files		*files;
+	t_buffer	*buffer;
+	size_t		final_return;
+	bool		use_canonical_style;
 
+	buffer = init_buffer();
+	if (buffer == NULL)
+		return (1);
 	(void)argc;
 	final_return = 0;
 	files = file_collector(++argv);
 	use_canonical_style = check_option_existence(argv);
 	if (files == 0)
-		final_return = stdin_dump(use_canonical_style);
+		final_return = handle_stdin(buffer, use_canonical_style);
 	else if (files != NULL)
 	{
-		final_return = files_dump(files, use_canonical_style);
+		final_return = handle_files(buffer, files, use_canonical_style);
 		free(files->paths);
 		free(files);
 	}
+	final_print(buffer, use_canonical_style);
+	free(buffer->current);
+	free(buffer->previous);
+	free(buffer);
 	return (final_return);
 }
