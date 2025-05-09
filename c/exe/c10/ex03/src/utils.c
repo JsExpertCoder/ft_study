38bfae06d7719ea38a329c5e6049757daabd80e5
JsExpertCoder <misterjs247000@gmail.com>
2025-05-09 17:37:41 +0100

    refactor: aggregate utilities in a single file

--- c/exe/c10/ex03/src/utils.c
+++ c/exe/c10/ex03/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: fnicolau <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 12:23:00 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/05/09 17:00:06 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/05/09 17:35:58 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,39 @@ char	*ft_strcpy(char *dst, const char *src)
 	}
 	dst[i] = 0;
 	return (dst);
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
+}
+
+void	ft_putstr_fd(char *str, int fd)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(fd, &str[i], 1);
+		i++;
+	}
+	return ;
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
 bool	print_hexa(size_t len, size_t nbr)
