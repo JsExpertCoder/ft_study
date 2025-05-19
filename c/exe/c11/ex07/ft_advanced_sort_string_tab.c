7ab911e77cdd9096852979bd9f180a4c8fec2cac
JsExpertCoder <misterjs247000@gmail.com>
2025-05-19 17:22:47 +0100

    chore: comment test code in ft_sort_string_tab

--- c/exe/c11/ex07/ft_advanced_sort_string_tab.c
+++ c/exe/c11/ex07/ft_advanced_sort_string_tab.c
@@ -6,21 +6,21 @@
 /*   By: fnicolau <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 15:50:26 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/05/19 17:17:48 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/05/19 17:21:47 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int	ft_strcmp(char *s1, char *s2)
-{
-	while (*s1 && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
-	return ((unsigned char)*s1 - (unsigned char)*s2);
-}
+// int	ft_strcmp(char *s1, char *s2)
+// {
+// 	while (*s1 && (*s1 == *s2))
+// 	{
+// 		s1++;
+// 		s2++;
+// 	}
+// 	return ((unsigned char)*s1 - (unsigned char)*s2);
+// }
 
 size_t	get_tab_len(char **tab)
 {
@@ -58,25 +58,25 @@ void	ft_advanced_sort_string_tab(char **tab, int(*cmp)(char *, char *))
 	}
 }
 
-int	main(void)
-{
-	size_t	i;
-	char	*tab[] = {"apple", "apPlE", "", "010", "1", "banana", "ABC", "42", "Orange", "007", NULL};
+// int	main(void)
+// {
+// 	size_t	i;
+// 	char	*tab[] = {"apple", "apPlE", "", "010", "1", "banana", "ABC", "42", "Orange", "007", NULL};
 
-	printf("> before sorting:\n");
-	i = 0;
-	while (tab[i])
-	{
-		printf("[%li] -> %s\n", i, tab[i]);
-		i++;
-	}
-	ft_advanced_sort_string_tab(tab, &ft_strcmp);
-	printf(">  after sorting:\n");
-	i = 0;
-	while (tab[i])
-	{
-		printf("[%li] -> %s\n", i, tab[i]);
-		i++;
-	}
-	return (0);
-}
+// 	printf("> before sorting:\n");
+// 	i = 0;
+// 	while (tab[i])
+// 	{
+// 		printf("[%li] -> %s\n", i, tab[i]);
+// 		i++;
+// 	}
+// 	ft_advanced_sort_string_tab(tab, &ft_strcmp);
+// 	printf(">  after sorting:\n");
+// 	i = 0;
+// 	while (tab[i])
+// 	{
+// 		printf("[%li] -> %s\n", i, tab[i]);
+// 		i++;
+// 	}
+// 	return (0);
+// }
