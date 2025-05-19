0042eab59908e5415b58e1d31d595eea7cd7c150
JsExpertCoder <misterjs247000@gmail.com>
2025-05-19 14:18:31 +0100

    chore: comment test code in ft_sort_string_tab

--- c/exe/c11/ex06/ft_sort_string_tab.c
+++ c/exe/c11/ex06/ft_sort_string_tab.c
@@ -6,21 +6,21 @@
 /*   By: fnicolau <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 12:36:38 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/05/19 13:25:25 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/05/19 14:17:43 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int	ft_strcmp(const char *s1, const char *s2)
-{
-	while (*s1 && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
-	return ((unsigned char)*s1 - (unsigned char)*s2);
-}
+// int	ft_strcmp(const char *s1, const char *s2)
+// {
+// 	while (*s1 && (*s1 == *s2))
+// 	{
+// 		s1++;
+// 		s2++;
+// 	}
+// 	return ((unsigned char)*s1 - (unsigned char)*s2);
+// }
 
 size_t	get_tab_len(char **tab)
 {
@@ -58,25 +58,25 @@ void	ft_sort_string_tab(char **tab)
 	}
 }
 
-int	main(void)
-{
-	size_t	i;
-	char	*tab[] = {"apple", "apPlE", "banana", "ABC", "42", "Orange", NULL};
+// int	main(void)
+// {
+// 	size_t	i;
+// 	char	*tab[] = {"apple", "apPlE", "banana", "ABC", "42", "Orange", NULL};
 
-	printf("> before sorting:\n");
-	i = 0;
-	while (tab[i])
-	{
-		printf("[%li] -> %s\n", i, tab[i]);
-		i++;
-	}
-	ft_sort_string_tab(tab);
-	printf(">  after sorting:\n");
-	i = 0;
-	while (tab[i])
-	{
-		printf("[%li] -> %s\n", i, tab[i]);
-		i++;
-	}
-	return (0);
-}
+// 	printf("> before sorting:\n");
+// 	i = 0;
+// 	while (tab[i])
+// 	{
+// 		printf("[%li] -> %s\n", i, tab[i]);
+// 		i++;
+// 	}
+// 	ft_sort_string_tab(tab);
+// 	printf(">  after sorting:\n");
+// 	i = 0;
+// 	while (tab[i])
+// 	{
+// 		printf("[%li] -> %s\n", i, tab[i]);
+// 		i++;
+// 	}
+// 	return (0);
+// }
