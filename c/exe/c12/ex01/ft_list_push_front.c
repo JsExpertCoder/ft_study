3a5362412235aef632f8028a2b82057b83780d47
JsExpertCoder <misterjs247000@gmail.com>
2025-05-20 19:18:41 +0100

    chore:  comment test code

--- c/exe/c12/ex01/ft_list_push_front.c
+++ c/exe/c12/ex01/ft_list_push_front.c
@@ -6,12 +6,12 @@
 /*   By: fnicolau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 17:55:14 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/05/20 18:32:19 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/05/20 19:18:06 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
-#include <stdio.h>
+// #include <stdio.h>
 
 void	ft_list_push_front(t_list **begin_list, void *data)
 {
@@ -30,60 +30,60 @@ void	ft_list_push_front(t_list **begin_list, void *data)
 	}
 }
 
-int	*get_data_ptr(int n)
-{
-	int	*ptr;
+// int	*get_data_ptr(int n)
+// {
+// 	int	*ptr;
 
-	ptr = malloc(sizeof(int));
-	*ptr = n;
-	return (ptr);
-}
+// 	ptr = malloc(sizeof(int));
+// 	*ptr = n;
+// 	return (ptr);
+// }
 
-t_list	*ft_create_elem(void *data)
-{
-	t_list	*elem;
+// t_list	*ft_create_elem(void *data)
+// {
+// 	t_list	*elem;
 
-	elem = malloc(sizeof(t_list));
-	if (!elem)
-		return (0);
-	elem->data = data;
-	elem->next = NULL;
-	return (elem);
-}
+// 	elem = malloc(sizeof(t_list));
+// 	if (!elem)
+// 		return (0);
+// 	elem->data = data;
+// 	elem->next = NULL;
+// 	return (elem);
+// }
 
-void	print_list(t_list *node)
-{
-	if (!node)
-		printf("> THE LIST IS EMPTY :(\n\n");
-	else
-	{
-		printf("Nodes in the List:\n");
-		while (node)
-		{
-			if (node->data)
-				printf("%i --> ", *(int *)node->data);
-			else
-				printf("(null) --> ");
-			node = node->next;
-		}
-	}
-}
+// void	print_list(t_list *node)
+// {
+// 	if (!node)
+// 		printf("> THE LIST IS EMPTY :(\n\n");
+// 	else
+// 	{
+// 		printf("Nodes in the List:\n");
+// 		while (node)
+// 		{
+// 			if (node->data)
+// 				printf("%i --> ", *(int *)node->data);
+// 			else
+// 				printf("(null) --> ");
+// 			node = node->next;
+// 		}
+// 	}
+// }
 
-int	main(void)
-{
-	t_list	*head;
+// int	main(void)
+// {
+// 	t_list	*head;
 
-	head = NULL;
-	print_list(head);
-	ft_list_push_front(&head, get_data_ptr(1));
-	ft_list_push_front(&head, NULL);
-	ft_list_push_front(&head, get_data_ptr(42));
-	ft_list_push_front(&head, get_data_ptr(82));
-	ft_list_push_front(&head, NULL);
-	ft_list_push_front(&head, get_data_ptr(1337));
-	ft_list_push_front(&head, NULL);
-	ft_list_push_front(&head, get_data_ptr(0));
-	ft_list_push_front(&head, NULL);
-	print_list(head);
-	return (0);
-}
+// 	head = NULL;
+// 	print_list(head);
+// 	ft_list_push_front(&head, get_data_ptr(1));
+// 	ft_list_push_front(&head, NULL);
+// 	ft_list_push_front(&head, get_data_ptr(42));
+// 	ft_list_push_front(&head, get_data_ptr(82));
+// 	ft_list_push_front(&head, NULL);
+// 	ft_list_push_front(&head, get_data_ptr(1337));
+// 	ft_list_push_front(&head, NULL);
+// 	ft_list_push_front(&head, get_data_ptr(0));
+// 	ft_list_push_front(&head, NULL);
+// 	print_list(head);
+// 	return (0);
+// }
