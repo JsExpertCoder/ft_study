cc57883ac8dfc10eaf0eab94dea6d636966caacf
JsExpertCoder <misterjs247000@gmail.com>
2025-05-21 15:49:52 +0100

    add(ex04): implement ft_list.h to handle libs inclusion functions declarations and a linked list definition (t_list)

---
+++ c/exe/c12/ex04/ft_list.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnicolau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 15:12:34 by fnicolau          #+#    #+#             */
+/*   Updated: 2025/05/21 15:14:39 by fnicolau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+# include <stdlib.h>
+
+typedef struct s_list
+{
+	struct s_list	*next;
+	void			*data;
+}	t_list;
+
+t_list	*ft_create_elem(void *data);
+#endif
