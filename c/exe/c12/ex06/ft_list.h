e216b9d419283035a5266718f0ace0e1800cd88f
JsExpertCoder <misterjs247000@gmail.com>
2025-05-22 23:02:00 +0100

    add(ex06): create ft_list.h with linked list definition and element creation function

---
+++ c/exe/c12/ex06/ft_list.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnicolau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/22 15:17:25 by fnicolau          #+#    #+#             */
+/*   Updated: 2025/05/22 15:19:29 by fnicolau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+# include <stdlib.h>
+
+typedef struct s_list
+{
+	struct s_list	*next;
+	void			*data;
+}	t_list;
+
+t_list	*ft_crete_elem(void *data);
+#endif
